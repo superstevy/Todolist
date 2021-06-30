5cf8e8df5e5c4fc4a4393e79921be67e8bf7c9b5
Stevy <superstevy@yahoo.com>
2021-06-30 18:52:45 -0400

    added api for register and login

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.contrib.auth import login
 from rest_framework import generics, permissions
 from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
 from .serializers import UserSerializer, RegisterSerializer
 
 # Register API
@@ -21,3 +23,16 @@ class RegisterAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
+
+# Login API
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
