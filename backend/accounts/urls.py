c61402d0fe03121b40d6f4eda3d5f59c4c6801dc
Stevy <superstevy@yahoo.com>
2021-07-07 16:52:15 -0400

    added user list

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/<int:pk>/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('api/user-list/', userList.as_view(), name='user-list'),
+    path('api/user-list/', userList, name='user-list'),
 ]
