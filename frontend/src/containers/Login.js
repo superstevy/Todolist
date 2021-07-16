77ad972728d9c1efa53a3cbc795e69a89217e4e0
Stevy <superstevy@yahoo.com>
2021-07-16 18:47:16 -0400

    testing logging in

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { Card, Alert } from 'react-bootstrap'
 
 import { Link, useHistory } from 'react-router-dom'
 
@@ -7,6 +7,9 @@ export default function Login () {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [message, setMessage] = React.useState('')
   const history = useHistory()
 
   const success = async (text) => {
@@ -17,13 +20,39 @@ export default function Login () {
   async function handleSubmit (e) {
     e.preventDefault()
 
-    const login = async (username, email, password, success, fail) => {}
+    const login = async (username, email, password, success, fail) => {
+      const url = 'https://todos-list-backends.herokuapp.com/api/login/'
+      const response = await fetch(url,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: username,
+            email: email,
+            password: password
+          })
+        })
+      const text = await response.text()
+      if (response.status === 200) {
+        console.log('Success', JSON.parse(text))
+        success(JSON.parse(text))
+      } else {
+        console.log('Failed', text)
+        Object.entries(JSON.parse(text)).forEach(([key, value]) => {
+          fail(`${key}: ${value}`)
+        })
+      }
+    }
+
     console.log('Loggin in with', username, email, password)
 
     try {
       setError('')
       setLoading(true)
-      await login(username, email, password, success)
+      await login(username, email, password, success, (text) => { setMessage(text) })
       history.push('/')
     } catch {
       setError('Failed to sign in')
@@ -36,21 +65,41 @@ export default function Login () {
     <div>
       <Card>
         <Card.Body>
-          <h2 className='text-center mb-4'>Log In</h2>
           {error && <Alert variant='danger'>{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id='email'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
-            </Form.Group>
-            <Button disabled={loading} className='w-100 mt-3' type='submit'>
-              Log In
-            </Button>
-          </Form>
+          <div style={{
+            width: '400px',
+            margin: 'auto',
+            marginTop: '200px',
+            boxShadow: '5px 5px 20px #cccccccc',
+            padding: '1em'
+          }}
+          >
+            <form>
+              <div className='mb-3'>
+                <label htmlFor='username' className='form-label'>Username</label>
+                <input
+                  autoFocus type='text' className='form-control' id='username' placeholder='username'
+                  onChange={(e) => { setUsername(e.target.value) }} value={username}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='email' className='form-label'>Email</label>
+                <input
+                  autoFocus type='email' className='form-control' id='email' placeholder='email'
+                  onChange={(e) => { setEmail(e.target.value) }} value={email}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='password' className='form-label'>Password</label>
+                <input
+                  type='password' className='form-control' id='password' placeholder='password'
+                  onChange={(e) => { setPassword(e.target.value) }} value={password}
+                />
+              </div>
+              <div style={{ margin: '1em', color: 'red' }}>{message}</div>
+              <button disabled={loading} type='submit' className='btn btn-primary' onClick={handleSubmit}>Login</button>
+            </form>
+          </div>
           <div className='w-100 text-center mt-3'>
             <Link to='/forgot-password'>Forgot Password?</Link>
           </div>
