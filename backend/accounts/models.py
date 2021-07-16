9d62646dc3da29156e43e60ab1f7d52bff72f089
Stevy <superstevy@yahoo.com>
2021-07-16 10:32:58 -0400

    small changes

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100, unique=True)
+    owner = models.ForeignKey(
+        User, related_name='accounts', on_delete=models.CASCADE, null=True)
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -11,23 +11,6 @@ export default function Login () {
   const [loading, setLoading] = useState(false)
   const history = useHistory()
 
-  const getCookie = (cookieName) => {
-    let cookieValue = null
-    if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';')
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim()
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, cookieName.length + 1) === (cookieName + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(cookieName.length + 1))
-          break
-        }
-      }
-    }
-
-    return cookieValue
-  }
-
   async function handleSubmit (e) {
     e.preventDefault()
 
